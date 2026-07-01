e2d0a58eaf7be6e475fc2315271ec686cb9efd58
demosche <misan3760@gmail.com>
2026-07-01 17:53:34 +0500

    created class req, that should take from url php info about group: type(idk what it is), id(of group), week

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from datetime import datetime
 
-from pydantic import BaseModel, PositiveInt
+from pydantic import BaseModel
 
 
 class Event(BaseModel):  model_config = ConfigDict(strict=True)
+
+class Req(BaseModel):
+    model_config = ConfigDict(strict=True)
+    type: int = 2
+    id: int
+    week: int = 0
